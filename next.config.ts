590dd0a9c8134b0ddc1247f9d73a74d3b9fdb46e
Zahid <zahidulislam.cs@gmail.com>
2026-08-21 11:45:56 +0600

    chore: github pages deployment configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: "/zverse",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
